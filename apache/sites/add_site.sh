32b69fff2fe2a60d20bf1cd5f1d33948e8f789a1
saul <saul.yeponde@gmail.com>
2022-04-16 20:52:38 +0200

    Fix issues
    Fixed the variable affectation.
    Fixed the sed command issue.

--- apache/sites/add_site.sh
+++ apache/sites/add_site.sh
@@ -5,7 +5,7 @@ read -p "Please enter the site directory: " siteDirectory
 read -p "Please enter the site server name eg www.my-site-name.com: " siteServerName
 read -p "Please enter the site configuration file name: " siteConfigurationFileName
 
-configurationFilePath = "/etc/apache/sites-available/$siteConfigurationFileName.conf"
+configurationFilePath="/etc/apache2/sites-available/$siteConfigurationFileName.conf"
 
 echo "Calculated configuration file path is : $configurationFilePath"
 
@@ -17,7 +17,7 @@ sudo chmod 644 $configurationFilePath
 echo "Attempting to replace {PORT_VALUE} by $port ..."
 sudo sed -i "s/{PORT_VALUE}/$port/" $configurationFilePath
 echo "Attempting to replace {SITE_DIRECTORY} by $siteDirectory ..."
-sudo sed -i "s/{SITE_DIRECTORY}/$siteDirectory/" $configurationFilePath
+sudo sed -i "s@{SITE_DIRECTORY}@$siteDirectory@" $configurationFilePath
 echo "Attempting to replace {SITE_SERVER_NAME} by $siteServerName ..."
 sudo sed -i "s/{SITE_SERVER_NAME}/$siteServerName/" $configurationFilePath
 
